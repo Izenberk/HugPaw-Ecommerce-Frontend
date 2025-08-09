0d63c2413c959c8b719cf9ebe14ad470b1bbdbca
Izenberk <aceskorn555@outlook.com>
2025-08-09 22:16:27 +0700

    fix(router): create new Router.jsx for router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './app/Layout'
-import Home from './pages/Home'
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: (
-      <div>
-        <h1>404 - Page Not Found ⚠️</h1>
-      </div>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-    ],
-  },
-]);
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/Router'
 
 function App() {
   return <RouterProvider router={router} />
