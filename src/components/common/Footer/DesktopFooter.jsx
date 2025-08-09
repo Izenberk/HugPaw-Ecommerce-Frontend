78fb04d72baaedaf32b32ada171f878ba27f720d
Izenberk <aceskorn555@outlook.com>
2025-08-09 18:42:43 +0700

    feat(footer): add footer + responsive

--- src/components/common/Footer/DesktopFooter.jsx
+++ src/components/common/Footer/DesktopFooter.jsx
@@ -1,7 +1,64 @@
+import Logo from "../Logo"
+import FBLogo from "../../../assets/images/logo/facebook.png"
+import IGLogo from "../../../assets/images/logo/instagram.png"
+import { Link } from "react-router-dom"
+
+
 const DesktopFooter = () => {
     return (
         <div className="max-w-6xl mx-auto hidden md:grid grid-cols-5 gap-6">
-            
+            <div className="hover:text-white hover:cursor-pointer">
+                <Logo />
+            </div>
+            <div>
+                <h3 className="font-bold mb-2">Company</h3>
+                <ul className="space-y-1 text-sm ">
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>About Us</Link>
+                    </li>
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>FAQ</Link>
+                    </li>
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Contact</Link>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 className="font-bold mb-2">Legal</h3>
+                <ul className="space-y-1 text-sm">
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Privacy Policy</Link>
+                    </li>
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Terms and Conditionsy</Link>
+                    </li>
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Cookie Policy</Link>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 className="font-bold mb-2">Help</h3>
+                <ul className="space-y-1 text-sm">
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Shipping and Delivery</Link>
+                    </li>
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Returns Policy</Link>
+                    </li>
+                    <li className="hover:text-white hover:cursor-pointer">
+                        <Link>Security and Payment</Link>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 className="font-bold mb-2">Social</h3>
+                <div className="flex gap-4">
+                    <Link><img src={FBLogo} alt="Facebook" className="h-5 w-5"/></Link>
+                    <Link><img src={IGLogo} alt="Instagram" className="h-5 w-5"/></Link>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,8 +1,12 @@
+import DesktopFooter from "./DesktopFooter"
+
 const Footer = () => {
     return (
-        <div className="bg-primary text-gray-800 px-6 py-8 h-[auto] md:h-[200px]">
-
-            <div className="mt-6 text-center text-sm text-primary-foreground">
+        <div className="bg-primary text-primary-foreground px-6 py-8 h-[auto] md:h-[200px]">
+            <div>
+                <DesktopFooter />
+            </div>
+            <div className="md:pt-6 text-center text-sm text-primary-foreground">
                 Copyright © HugPaw 2025
             </div>
         </div>
