7b6aa1b03e31bc10fc84444afba1b492212a5d36
nongnapasbb <nongnapasbb@gmail.com>
2025-08-14 09:56:39 +0700

    feat(home): add CategoryRow section for product categories

---
+++ src/components/home/CategoryRow.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
+export default function CategoryRow() {
+  return (
+    <section className="mx-auto max-w-6xl px-6 py-10 bg-primary">
+      <div className="mb-6">
+        <h2 className="text-2xl font-bold">Our Products</h2>
+        <p className="text-sm text-muted-foreground"></p>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3">
+          <Card className="text-center">
+            <img
+              src="/src/assets/images/products/collar.jpg"
+              alt="Pet Collar"
+              className="w-full h-48 object-contain p-4"
+            />
+            <CardHeader>
+              <CardTitle>Collar</CardTitle>
+              <p className="text-sm text-muted-foreground">More than just a collar-It's their identity</p>
+            </CardHeader>
+            <CardContent className="flex justify-center mt-auto">
+              <Button variant="accent" asChild className="bg-black hover:bg-purple-300 text-white">
+                <a href="#">Let’s Customize</a>
+              </Button>
+            </CardContent>
+
+          </Card>
+           <Card className="text-center">
+             <img
+              src="/src/assets/images/products/water-dispenser.jpg"
+              alt="Water Dispenser"
+              className="w-full h-48 object-contain p-4"
+            />
+            <CardHeader>
+              <CardTitle>Water Dispenser</CardTitle>
+              <p className="text-sm text-muted-foreground">Their wellness starts with water</p>
+            </CardHeader>
+            <CardContent className="flex justify-center mt-auto">
+              <Button variant="secondary" asChild className="bg-black hover:bg-purple-300 text-white">
+                <a href="#">Let’s Customize</a>
+              </Button>
+            </CardContent>
+          </Card>
+
+           <Card className="text-center">
+             <img
+              src="/src/assets/images/products/feeder.jpg"
+              alt="Feeder"
+              className="w-full h-48 object-contain p-4"
+            />
+            <CardHeader>
+              <CardTitle>Feeder</CardTitle>
+              <p className="text-sm text-muted-foreground">Mealtime made personal</p>
+            </CardHeader>
+            <CardContent className="flex justify-center mt-auto">
+              <Button variant="secondary" asChild className="bg-black hover:bg-purple-300 text-white">
+                <a href="#">Let’s Customize</a>
+              </Button>
+            </CardContent>
+          </Card>
+      </div>
+    </section>
+  );
+}
