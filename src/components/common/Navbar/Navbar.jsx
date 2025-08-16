d50571122848a383a76f8f162d1f586b21f9d86a
Oranut Suphantamat <Oranut_s@kkumail.com>
2025-08-16 22:47:35 +0700

    Feat(MobileNav): Edit to referance

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,19 +1,17 @@
-import DesktopNav from "@components/common/Navbar/DesktopNav"
-import MobileNav from "@components/common/Navbar/MobileNav"
-
-
+import DesktopNav from "@components/common/Navbar/DesktopNav";
+import MobileNav from "@components/common/Navbar/MobileNav";
 
 const Navbar = () => {
-    return (
-        <div className="bg-primary text-primary-foreground h-[80px] px-4 flex">
-            <div className='hidden md:flex items-center justify-between w-full'>
-                <DesktopNav />
-            </div>
-            <div className='flex items-center justify-between w-full md:hidden'>
-                <MobileNav />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-primary text-primary-foreground h-[80px] px-4 flex">
+      <div className="hidden md:flex items-center justify-between w-full">
+        <DesktopNav />
+      </div>
+      <div className="flex items-center justify-between w-full md:hidden">
+        <MobileNav />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
