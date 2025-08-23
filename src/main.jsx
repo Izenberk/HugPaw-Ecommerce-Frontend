3e13f2b28e6a46265551c82aad56bc98cebe0f60
Izenberk <aceskorn555@outlook.com>
2025-08-23 23:14:18 +0700

    chore: rename pages/UserPage -> pages/userPage (case-sensitive)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { TooltipProvider } from "./components/ui/tooltip";
-import { AuthProvider } from "./pages/auth/AuthContext";
-import { CartProvider } from "./pages/userCart/CartContext";
+import "@/index.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AuthProvider } from "@/pages/auth/AuthContext";
+import { CartProvider } from "@/pages/userCart/CartContext";
+import App from "@/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
