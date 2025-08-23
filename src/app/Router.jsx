d9de3294c30f623fc2ffff6504a6b0ecf74b984d
Izenberk <aceskorn555@outlook.com>
2025-08-23 21:30:02 +0700

    fix: case-sensitive path for UserPage on Vercel

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '@/app/Layout'
 import Home from '@/pages/Home'
-import UserPage from '@/pages/userPage/UserPage';
 import Login from '@/pages/auth/Login';
 import ProductCatalogPage from '@/pages/productCatalog/ProductRoute';
 import Checkout from '@/pages/Checkout/Checkout';
@@ -9,6 +8,7 @@ import Cart from '@/pages/userCart/Cart';
 import ProductDetailRoute, { productLoader } from '@/pages/productDetail/ProductDetailRoute';
 import Signup from '@/pages/auth/SignUp';
 import NotFound from '@/pages/404_page';
+import UserPage from '@/pages/UserPage/UserPage';
 
 
 export const router = createBrowserRouter([
