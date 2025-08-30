44890c90036d2553e14190efe42a664f4d437b5f
Izenberk <aceskorn555@outlook.com>
2025-08-30 15:40:47 +0700

    feat(page): add scroll restoration to page structure

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,19 +1,25 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../components/common/Navbar/Navbar"
-import Footer from "../components/common/Footer/Footer"
-
-
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import Navbar from "../components/common/Navbar/Navbar";
+import Footer from "../components/common/Footer/Footer";
 
 const Layout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-background text-foreground">
+        {/* Fixed navbar on top */}
+        <header className="fixed top-0 inset-x-0 z-50 bg-background/80 backdrop-blur border-b">
             <Navbar />
-            <div className=" flex-1">
-                <Outlet />
-            </div>
-            <Footer />
+        </header>
+
+        {/* Push content below the fixed navbar (adjust pt-14 to your navbar height) */}
+        <main className="flex-1 pt-[80px] md:px-[100px]">
+            {/* Scroll restoration (back/forward = restore; new nav can pair with TopOnNavigate below) */}
+            <ScrollRestoration/>
+            <Outlet />
+        </main>
+
+        <Footer />
         </div>
     );
-}
+};
 
-export default Layout
+export default Layout;
