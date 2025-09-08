8a073cd0d55a895c4caf7ae50e8a12110f74d58e
Izenberk <aceskorn555@outlook.com>
2025-09-08 13:50:41 +0700

    deploy on render

--- src/components/productCustom/ProductCustom.jsx
+++ src/components/productCustom/ProductCustom.jsx
@@ -7,7 +7,7 @@ import { useLocation } from "react-router-dom";
 import { useAddToCartToast } from "@/hooks/useAddToCartToast";
 import { toast } from "sonner";
 import { useCart } from "@/context/CartContext";
-import { getGroup } from "@/lib/productOptions";
+
 
 export default function ProductCustom({ product, onAddToCart }) {
     const addToCartToast = useAddToCartToast({ onAdd: onAddToCart });
