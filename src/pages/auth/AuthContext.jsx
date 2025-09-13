e7a0e7d3f7ce0bb706dc2c7d69e3d12c76b00c31
Oranut Suphantamat <Oranut_s@kkumail.com>
2025-09-13 20:26:02 +0700

    edit(AuthContext): add state and logic to link with back-end

--- src/pages/auth/AuthContext.jsx
+++ src/pages/auth/AuthContext.jsx
@@ -1,17 +1,69 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const login = () => setIsLoggedIn(true);
-  const logout = () => setIsLoggedIn(false);
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch("http://localhost:3030/api/v1/auth/me", {
+          credentials: "include",
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data.user);
+        } else {
+          setUser(null);
+        }
+      } catch (err) {
+        console.error("Auth check failed:", err);
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
 
+  const login = async (email, password) => {
+    try {
+      const res = await fetch("http://localhost:3030/api/v1/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", // ให้ cookie accessToken กลับมา
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.message || "Login failed");
+
+      setUser(data.user);
+      return { success: true };
+    } catch (err) {
+      return { success: false, message: err.massage };
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await fetch("http://localhost:3030/api/v1/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+    } catch (err) {
+      console.error("Logout failed", err);
+    } finally {
+      setUser(null);
+    }
+  };
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider
+      value={{ user, isLoggedIn: !!user, login, logout, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
