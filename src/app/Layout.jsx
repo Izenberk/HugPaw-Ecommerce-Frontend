0638a0e4d4cc8497d871a15d7f1c8ab7e834f135
Nongnapas Thongruang <nongnapasbb@gmail.com>
2025-09-13 16:16:17 +0700

    feat(layout): refactor layout structure for improved header organization

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,27 +1,27 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import Topbar from "../components/common/Topbar/Topbar";
 import Navbar from "../components/common/Navbar/Navbar";
 import Footer from "../components/common/Footer/Footer";
 import { ToastViewport } from "@/lib/toast";
 
 const Layout = () => {
-    return (
-        <div className="min-h-screen flex flex-col bg-background text-foreground">
-            {/* Fixed navbar on top */}
-            <header className="fixed top-0 inset-x-0 z-50 bg-background/80 backdrop-blur border-b">
-                <Navbar />
-            </header>
+  return (
+    <div className="">
+      <header>
+        <Topbar />
+        <Navbar />
+      </header>
 
-            {/* Push content below the fixed navbar (adjust pt-14 to your navbar height) */}
-            <main className="flex-1 pt-[80px] md:px-[100px]">
-                {/* Scroll restoration (back/forward = restore; new nav can pair with TopOnNavigate below) */}
-                <ScrollRestoration/>
-                <Outlet />
-            </main>
+      {/* Push content below (Topbar + Navbar height) */}
+      <main className="flex-1 pt-[120px] md:px-[100px]">
+        <ScrollRestoration />
+        <Outlet />
+      </main>
 
-            <Footer />
-            <ToastViewport/>
-        </div>
-    );
+      <Footer />
+      <ToastViewport />
+    </div>
+  );
 };
 
 export default Layout;
