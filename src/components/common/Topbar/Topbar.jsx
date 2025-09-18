370b08ee71ed6aa592e6ac7e59a4c78a9081dd9d
Izenberk <aceskorn555@outlook.com>
2025-09-18 07:57:37 +0700

    fix(topbar): change promotion

--- src/components/common/Topbar/Topbar.jsx
+++ src/components/common/Topbar/Topbar.jsx
@@ -3,7 +3,7 @@ const Topbar = () => {
     <div className="top-0 left-0 w-full z-50 bg-gray-800 text-white text-sm">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-center gap-1 md:gap-3 py-2 px-4">
         <span className="text-center">
-          🚚 Free shipping on orders over ฿4,000
+          🎉 🎉 Use promo code "HappyHugPaw" for 5% off 🎉 🎉
         </span>
         <a
           href="/catalog"
